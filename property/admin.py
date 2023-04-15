d2675d15c4955b4bf76097390a6a207a052ff7c5
Balumys <123511478+Balumys@users.noreply.github.com>
2023-04-15 16:21:31 +0300

    Made data-migrations to normalize phone numbers

--- property/admin.py
+++ property/admin.py
@@ -6,7 +6,7 @@ from .models import Flat, Complaint
 class AccountAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address')
     readonly_fields = ['created_at']
-    list_display = ('address', 'price', 'new_building', 'construction_year')
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'owners_phonenumber', 'owner_pure_phone')
     list_editable = ['new_building']
     list_filter = ('new_building', 'rooms_number', 'floor', 'has_balcony', 'active')
     raw_id_fields = ('liked_by',)
